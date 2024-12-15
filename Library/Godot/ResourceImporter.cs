de01f9f6fd6fd5231c498e283b731deee12b77cf
RustyRoboticsBV <rusty.robotics.bv@gmail.com>
2024-12-15 14:26:49 +0100

    Added folder for Godot-exclusive code and renamed resource importer script.

---
+++ Library/Godot/ResourceImporter.cs
@@ -0,0 +1,39 @@
+﻿#if GODOT
+using Godot;
+using Godot.Collections;
+
+namespace Rusty.Csv
+{
+    /// <summary>
+    /// A base class for resource importers that can load a CSV table from a file and convert it to the desired resource type.
+    /// </summary>
+    public abstract partial class CsvResourceImporter<ResourceT> : Node where ResourceT : Resource
+    {
+        /* Public methods. */
+        /// <summary>
+        /// Load a resource from a file.
+        /// </summary>
+        public ResourceT Import(string filePath, Dictionary importOptions = null)
+        {
+            CsvTable table = CsvTable.Load(filePath);
+            return Convert(table, importOptions);
+        }
+
+        /// <summary>
+        /// Load a resource from a file, using some importer.
+        /// </summary>
+        public static ResourceT Import<ImporterT>(string filePath, Dictionary importOptions = null)
+            where ImporterT : CsvResourceImporter<ResourceT>, new()
+        {
+            ImporterT importer = new ImporterT();
+            return importer.Import(filePath, importOptions);
+        }
+
+        /* Protected methods. */
+        /// <summary>
+        /// Convert a loaded CSV table into a resource.
+        /// </summary>
+        protected abstract ResourceT Convert(CsvTable table, Dictionary importOptions);
+    }
+}
+#endif
